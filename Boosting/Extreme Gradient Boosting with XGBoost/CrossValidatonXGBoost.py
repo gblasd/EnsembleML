7b433ee0f9a8e4ffe5036ab4eae4416977ad53a3
Gustavo Blas Duran <gustavoblasduran@gmail.com>
2025-02-12 23:12:24 -0600

    Objective loss functions and base learners

---
+++ Boosting/Extreme Gradient Boosting with XGBoost/CrossValidatonXGBoost.py
@@ -0,0 +1,20 @@
+import xgboost as xgb
+import pandas as pd
+
+churn_data = pd.read_csv('classification_data.csv')
+churn_dmatrix = xgb.DMatrix(data=churn_data.iloc[:,:-1],
+                            label=churn_data.month_5_still_here)
+
+params = { 
+    "objective": "binary:logistic",
+    "max_depth": 4
+    }
+
+cv_results = xgb.cv(dtrain=churn_dmatrix,
+                    params=params,
+                    nfold=4,
+                    num_boost_round=10,
+                    metrics="error",
+                    as_pandas=True)
+
+print("Accuracy: %f" %((1-cv_results["test-error-mean"]).iloc[-1]))
